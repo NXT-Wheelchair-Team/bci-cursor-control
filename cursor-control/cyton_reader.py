49cdaf7179ee0e4cfc3b31862f809786ccd71a55
Adam Del Rosso <avdonle@outlook.com>
2021-03-01 13:11:15 -0500

    Add working brainflow cyton test

--- cursor-control/cyton_reader.py
+++ cursor-control/cyton_reader.py
@@ -1,9 +1,25 @@
-from brainflow.board_shim import BoardShim, BoardIds
+import time
+
+from brainflow.board_shim import BoardShim, BoardIds, BrainFlowInputParams
 
 
 def main():
     BoardShim.enable_dev_board_logger()
-    board = BoardShim(BoardIds.CYTON_BOARD, )
+    board_params = BrainFlowInputParams()
+    board_params.serial_port = "/dev/ttyUSB0"
+    board = BoardShim(BoardIds.CYTON_BOARD, board_params)
+    board.prepare_session()
+
+    board.start_stream(
+        num_samples=250 * 5
+    )  # no block, num samples is length of rolling buffer
+    time.sleep(10)
+    data = board.get_board_data()
+    board.stop_stream()
+    board.release_session()
+
+    print(data)
+    print(len(data[0]) / 250)
 
 
 if __name__ == "__main__":
