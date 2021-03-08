ae259232dfe6347f93b1f49bada85725b34bf129
Adam Del Rosso <avdonle@outlook.com>
2021-03-08 17:44:37 -0500

    Add bar plot Tk wrapper

--- cursor-control/expirement_gui/tk_plots.py
+++ cursor-control/expirement_gui/tk_plots.py
@@ -9,25 +9,52 @@ import numpy as np
 class TkPlot:
     def __init__(self, canvas: sg.tk.Canvas):
         self.canvas = canvas
-        fig, self.axis = plt.subplots()
+        fig, self.axes = plt.subplots()
         self.figure = FigureCanvasTkAgg(fig, self.canvas)
         self.figure.draw()
         self.figure.get_tk_widget().pack(side="top", fill="both", expand=1)
 
 
 class LinePlot(TkPlot):
+    """
+    Wrapper around matplotlib.pyplot.plot for Tk. Designed to be updated many times within the same canvas.
+    """
+
     def __init__(self, canvas: sg.tk.Canvas):
         super(LinePlot, self).__init__(canvas)
 
-    def update(self, x: np.array, y: np.array):
-        self.axis.cla()
-        self.axis.plot(x, y)
+    def plot(self, *args, **kwargs):
+        self.axes.cla()
+        self.axes.plot(*args, **kwargs)
+        self.figure.draw()
+
+
+class BarPlot(TkPlot):
+    """
+    Wrapper around matplotlib.pyplot.bar for Tk. Designed to be updated many times within the same canvas.
+    """
+
+    def __init__(
+        self,
+        canvas: sg.tk.Canvas,
+    ):
+        super(BarPlot, self).__init__(canvas)
+
+    def plot(self, *args, **kwargs):
+        self.axes.cla()
+        self.axes.bar(*args, **kwargs)
+        self.axes.set_ylim([0, 1])
         self.figure.draw()
 
 
 if __name__ == "__main__":
     layout = [
-        [sg.Canvas(size=(640, 480), key="-CANVAS-")],
+        [
+            sg.Canvas(size=(640, 480), key="line"),
+        ],
+        [
+            sg.Canvas(size=(640, 480), key="bar"),
+        ],
     ]
 
     # create the form and show it without the plot
@@ -35,12 +62,18 @@ if __name__ == "__main__":
         "Demo Application - Embedding Matplotlib In PySimpleGUI", layout, finalize=True
     )
 
-    canvas_elem = window["-CANVAS-"]
-    canvas = canvas_elem.TKCanvas
-    line_plot = LinePlot(canvas)
+    canvas_elem_line = window["line"]
+    canvas_line = canvas_elem_line.TKCanvas
+    line_plot = LinePlot(canvas_line)
+
+    canvas_elem_bar = window["bar"]
+    canvas_bar = canvas_elem_bar.TKCanvas
+    bar_plot = BarPlot(canvas_bar)
 
     while True:
-        time.sleep(0.033)
+        time.sleep(0.01)
         x = range(0, 100)
         y = np.random.rand(100)
-        line_plot.update(x, y)
+        bar_height = np.random.rand(1)
+        line_plot.plot(x, y, alpha=0.5)
+        bar_plot.plot(0, bar_height)
